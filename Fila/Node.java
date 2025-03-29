98d4ed7f7fcafc4abe278ef659242e949ca87dc8
Bryan Strey <137365683+joystick2707@users.noreply.github.com>
2025-03-29 15:15:37 -0300

    Create Node.java

---
+++ Fila/Node.java
@@ -0,0 +1,11 @@
+//Trabalho de Bryan Strey, Victor Luis
+
+class Node {
+    int valor;
+    Node proximo;
+
+    public Node(int valor) {
+        this.valor = valor;
+        this.proximo = null;
+    }
+}
